b7ac8289bb635b12e4612bea2bcdf30543a1a763
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2025-04-29 06:21:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
 from modules.start import start_handler  # Change to the actual function name
-from modules.warn import warn_handlers
-from modules.nsfw_sticker import nsfw_handlers
+from modules.warn import warn_handler
+from modules.nsfw_sticker import nsfw_handler
 
 app = Client(
     "AbuseRemoverBot",
